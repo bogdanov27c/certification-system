bd773ce0774e1200fc3ef87b283925aab21d00e4
bogdanov27c <zhen2ya7@yandex.ru>
2022-08-29 16:07:37 +0300

    refactoring

--- srv/certificate-support-member.js
+++ srv/certificate-support-member.js
@@ -1,68 +1,71 @@
 const cds = require('@sap/cds');
 const { Certificates, Exams, Results } = cds.entities;
 
-module.exports = cds.service.impl(srv => {
+module.exports = cds.service.impl(async srv => {
+    srv.after('CREATE', 'Results', async (data) => afterCreateResult(data));
+    srv.after('UPDATE', 'Results', async (data) => afterUpdateResult(data));
+});
 
-    srv.after('CREATE', 'Results', async (data) => {
-        let certificate;
-        if (data.status === 'PASSED') {
-            const getCertificationIdQuery = SELECT.from(Exams)
-                .columns('certificationID_ID')
-                .where({ ID: data.exam_ID });
-            const certificationId = await cds.run(getCertificationIdQuery);
+async function afterCreateResult(data) {
+    let certificate;
+    if (data.status === 'PASSED') {
+        const getCertificationIdQuery = SELECT.from(Exams)
+            .columns('certificationID_ID')
+            .where({ID: data.exam_ID});
+        const certificationId = await cds.run(getCertificationIdQuery);
 
-            const newCertQuery = INSERT.into(Certificates, [{
-                holder_ID: data.achiever_ID,
-                certification_ID: certificationId[0].certificationID_ID,
-                achievedAt: new Date(data.examinedAt),
-                valid: true
-            }]);
-            certificate = await cds.run(newCertQuery);
-            return certificate.req.data;
-        }
-    });
-
-    srv.after('UPDATE', 'Results', async (req) => {
-        if (req.status) {
-            const resultStatus = await getResultStatusById(req.ID);
-            if (resultStatus !== req.status) {
-                const results = {
-                    PASSED: true,
-                    FAILED: false
-                };
-                const isCertValid = results[resultStatus[0].status];
-                await updateCertificateByResultId(req.ID, { valid: isCertValid })
-            }
-        }
-        if (req.examinedAt) {
-            const isCertExpired = _diffBetweenDatesInMonths(req.examinedAt, new Date()) > 6;
-            if (isCertExpired) {
-                await updateCertificateByResultId(req.ID, { valid: false })
-            }
-        }
-    })
-
-    async function updateCertificateByResultId(resultId, data) {
-        const getCertIdQuery = SELECT.from(Certificates)
-            .columns('ID')
-            .where({ resultId_ID: resultId });
-        const certId = await cds.run(getCertIdQuery);
-
-        const updateCertQuery = UPDATE(Certificates, {ID: certId}).with(data);
-        await cds.run(updateCertQuery);
+        const newCertQuery = INSERT.into(Certificates, [{
+            holder_ID: data.achiever_ID,
+            certification_ID: certificationId[0].certificationID_ID,
+            achievedAt: new Date(data.examinedAt),
+            valid: true,
+            resultId_ID: data.ID
+        }]);
+        certificate = await cds.run(newCertQuery);
+        return certificate.req.data;
     }
+}
 
-    async function getResultStatusById(resultId) {
-        const getResultIdQuery = SELECT.from(Results)
-            .columns('status')
-            .where({ ID: resultId });
-        return await cds.run(getResultIdQuery);
+async function afterUpdateResult(data) {
+    if (data.status) {
+        const resultStatus = await getResultStatusById(data.ID);
+        if (resultStatus !== data.status) {
+            const results = {
+                PASSED: true,
+                FAILED: false
+            };
+            const isCertValid = results[resultStatus[0].status];
+            await updateCertificateByResultId(data.ID, {valid: isCertValid})
+        }
     }
+    if (data.examinedAt) {
+        const isCertExpired = _diffBetweenDatesInMonths(data.examinedAt, new Date()) > 6;
+        if (isCertExpired) {
+            await updateCertificateByResultId(data.ID, {valid: false})
+        }
+    }
+}
 
-    function _diffBetweenDatesInMonths(date1, date2) {
-        const avgNumberOfDaysInMonth = 30.417;
-        const diffTime = Math.abs(new Date(date1) - new Date(date2));
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-        return diffDays / avgNumberOfDaysInMonth;
-    }
-})
+async function updateCertificateByResultId(resultId, data) {
+    const getCertIdQuery = SELECT.from(Certificates)
+        .columns('ID')
+        .where({resultId_ID: resultId});
+    const certId = await cds.run(getCertIdQuery);
+
+    const updateCertQuery = UPDATE(Certificates, {ID: certId}).with(data);
+    await cds.run(updateCertQuery);
+}
+
+async function getResultStatusById(resultId) {
+    const getResultIdQuery = SELECT.from(Results)
+        .columns('status')
+        .where({ID: resultId});
+    return await cds.run(getResultIdQuery);
+}
+
+function _diffBetweenDatesInMonths(date1, date2) {
+    const avgNumberOfDaysInMonth = 30.417;
+    const diffTime = Math.abs(new Date(date1) - new Date(date2));
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays / avgNumberOfDaysInMonth;
+}
